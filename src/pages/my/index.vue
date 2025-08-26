cd1a9f15dcbc95c590d154726861093c07542e68
Bruce <recall4056@gmail.com>
2025-08-26 22:03:54 +0800

    feat: :sparkles: my page

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import type { TokenUser } from '@/api/auth/types'
+
+const userInfo = ref<TokenUser | null>(null)
+
 const logout = () => {
   uni.showToast({
     title: '退出成功',
@@ -6,18 +10,27 @@ const logout = () => {
   })
   uni.redirectTo({ url: '/pages/index/index' })
 }
+
+onShow(() => {
+  userInfo.value = uni.getStorageSync('user') as TokenUser
+})
 </script>
 
 <template>
   <view class="page">
-    <wd-button @click="logout">注销</wd-button>
+    <text>{{ userInfo?.UserName }}</text>
+    <view style="margin-top: 12px">
+      <wd-button @click="logout">注销</wd-button>
+    </view>
   </view>
 </template>
 
 <style scoped lang="scss">
 .page {
-  padding-top: 20px;
+  padding-top: 16px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 }
 </style>
